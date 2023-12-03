8af65a23502a7ac25a005a44c165ae5ea63c6152
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-12-03 11:14:40 +0300

    init: start task coffee-house-week-3

--- coffee-house/pages/main/script.js
+++ coffee-house/pages/main/script.js
@@ -6,4 +6,22 @@ window.addEventListener('load', (event) => {
 
 console.log('Coffee House - main page');
 
-//coffee-house-week2
+//coffee-house-week3
+
+// openOrCloseBurgerMenu
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerLine1 = document.querySelector('.burger-line-1');
+const burgerLine2 = document.querySelector('.burger-line-2');
+burgerBtn.addEventListener('click', event => openOrCloseBurgerMenu());
+function openOrCloseBurgerMenu() {
+    if (burgerMenu.classList.contains('burger-menu-opened')) {
+        burgerMenu.classList.remove('burger-menu-opened');
+        burgerLine1.classList.remove('burger-line-to-cross-1');
+        burgerLine2.classList.remove('burger-line-to-cross-2');
+    } else {
+        burgerMenu.classList.add('burger-menu-opened');
+        burgerLine1.classList.add('burger-line-to-cross-1');
+        burgerLine2.classList.add('burger-line-to-cross-2');
+    }
+}
--- coffee-house/pages/menu/script.js
+++ coffee-house/pages/menu/script.js
@@ -3,3 +3,23 @@ window.addEventListener('load', (event) => {
     document.querySelector('.page').style.opacity = 1;
 });
 console.log('Coffee House - menu page');
+
+//coffee-house-week3
+
+// openOrCloseBurgerMenu
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerLine1 = document.querySelector('.burger-line-1');
+const burgerLine2 = document.querySelector('.burger-line-2');
+burgerBtn.addEventListener('click', event => openOrCloseBurgerMenu());
+function openOrCloseBurgerMenu() {
+    if (burgerMenu.classList.contains('burger-menu-opened')) {
+        burgerMenu.classList.remove('burger-menu-opened');
+        burgerLine1.classList.remove('burger-line-to-cross-1');
+        burgerLine2.classList.remove('burger-line-to-cross-2');
+    } else {
+        burgerMenu.classList.add('burger-menu-opened');
+        burgerLine1.classList.add('burger-line-to-cross-1');
+        burgerLine2.classList.add('burger-line-to-cross-2');
+    }
+}
