6221912baf42fff30fcee3ea16336e56cd424402
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-01-22 16:19:35 +0300

    feat: task 04-copy-directory is complete

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+const origPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
+
+function copyDir(origPath, copyPath) {
+  fs.mkdir(copyPath, { recursive: true }, () => {});
+  fs.readdir(origPath, (err, files) => {
+    files.forEach((file) => {
+      fs.copyFile(`${origPath}/${file}`, `${copyPath}/${file}`, () => {});
+    });
+  });
+}
+
+copyDir(origPath, copyPath);
