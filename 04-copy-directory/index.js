aa3590e5875efc4936b98ceed1fa3870ae9dfeaa
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-01-23 11:01:36 +0300

    fix: task 04 add rm files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,10 +5,12 @@ const origPath = path.join(__dirname, 'files');
 const copyPath = path.join(__dirname, 'files-copy');
 
 function copyDir(origPath, copyPath) {
-  fs.mkdir(copyPath, { recursive: true }, () => {});
-  fs.readdir(origPath, (err, files) => {
-    files.forEach((file) => {
-      fs.copyFile(`${origPath}/${file}`, `${copyPath}/${file}`, () => {});
+  fs.rm(copyPath, { recursive: true, force: true }, () => {
+    fs.mkdir(copyPath, { recursive: true }, () => {});
+    fs.readdir(origPath, (err, files) => {
+      files.forEach((file) => {
+        fs.copyFile(`${origPath}/${file}`, `${copyPath}/${file}`, () => {});
+      });
     });
   });
 }
