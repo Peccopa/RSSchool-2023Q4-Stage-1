21c8938c31a9e4733f1dc38d85203bc16568d674
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-01-30 09:45:30 +0300

    feat: add game main logic

--- nonograms/script.js
+++ nonograms/script.js
@@ -1,4 +1,11 @@
-'use strict';
+import { templates } from './data.js';
+// import { test } from './data.js';
+// import { keyboardArray, data, scaffoldSVGinner, humanParts } from "./data.js";
+
+// console.log(test);
+
+
+// ('use strict');
 
 window.addEventListener('load', () => {
   setTimeout(() => {
@@ -7,6 +14,11 @@ window.addEventListener('load', () => {
 });
 
 //Create game-box
+const cells = 5;
+const frames = 5;
+const fieldArr = Array.from(Array(cells * 3), () =>
+  new Array(cells * 3).fill(0)
+);
 const gameBox = document.createElement('div');
 gameBox.className = 'game-box';
 document.body.append(gameBox);
@@ -56,35 +68,51 @@ function createGameField(cells) {
   const gameField = document.createElement('div');
   gameField.className = 'game-field';
   gameBox.append(gameField);
+  let cellId = 0;
   for (let i = 0; i < 9; i += 1) {
     const gameFrame = document.createElement('div');
     gameFrame.className = 'game-frame';
     gameFrame.id = `fr-${i}`;
     gameField.append(gameFrame);
     gameFrame.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
-    for (let j = 0; j < cells * cells; j += 1) {
-      const gameCell = document.createElement('div');
-      gameCell.className = 'game-cell';
-      gameCell.id = `cell-${j + i * cells * 5}`;
-      gameFrame.append(gameCell);
-      gameCell.addEventListener('click', (cell) => {
-        clickOnCell(cell.target);
-      });
-      createFieldArray();
+    let lastTop = 0;
+    for (let k = 0; k < cells; k += 1) {
+      for (let j = 0; j < cells; j += 1) {
+        const gameCell = document.createElement('div');
+        gameCell.className = 'game-cell';
+        gameCell.id = cellId;
+        gameFrame.append(gameCell);
+        gameCell.addEventListener('click', (cell) => {
+          clickOnCell(cell.target);
+        });
+        cellId += 1;
+        if (k === 0 && j === cells - 1) lastTop = cellId;
+      }
+      cellId += cells * 2;
     }
+    i === 2 || i === 5 ? (cellId = cellId - cells * 2) : (cellId = lastTop);
   }
 }
 
-function createFieldArray() {
-
-}
-
 function clickOnCell(cell) {
-  cell.classList.toggle('active-cell');
+  const row = Math.floor(cell.id / (cells * 3));
+  const col = cell.id - row * (cells * 3);
+
+  if (cell.classList.contains('active-cell')) {
+    cell.classList.remove('active-cell');
+    fieldArr[row][col] = 0;
+  } else {
+    cell.classList.add('active-cell');
+    fieldArr[row][col] = 1;
+  }
+
+  if (JSON.stringify(fieldArr) === JSON.stringify(templates.temp5x5_1)) {
+    alert('win!');
+  }
+  // console.log(JSON.stringify(fieldArr));
+  // console.log(JSON.stringify(templates.temp5x5_1));
 }
 
-
-const cells = 5;
 createGameField(cells);
 createTopPanel(cells);
 createLeftPanel(cells);
