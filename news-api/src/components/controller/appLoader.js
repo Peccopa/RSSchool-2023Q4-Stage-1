02af7e5cd655c5e3f992f01db7e1ebe257540a84
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-02-21 13:05:29 +0300

    feat: add api key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -4,6 +4,7 @@ class AppLoader extends Loader {
     constructor() {
         super(process.env.API_URL, {
             apiKey: process.env.API_KEY,
+            // apiKey: '871bc7652cef480490494f2a62d70757',
         });
     }
 }
